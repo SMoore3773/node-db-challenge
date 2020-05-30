a28616112c43bfa77af451bd24fb5c2697817da5
SMoore3773 <Smooresculpture@gmail.com>
2020-05-30 15:08:29 -0400

    fin

--- data/seeds/project-seed.js
+++ data/seeds/project-seed.js
@@ -6,8 +6,8 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('projects').insert([
         {id: 1, project_name: 'laundry', project_desc:'clean cloths', project_complete:false},
-        {id: 1, project_name: 'carwash', project_desc:'clean car', project_complete:true},
-        {id: 1, project_name: 'mop floor', project_desc:'clean floor', project_complete:false}
+        {id: 2, project_name: 'carwash', project_desc:'clean car', project_complete:true},
+        {id: 3, project_name: 'mop floor', project_desc:'clean floor', project_complete:false}
       ]);
     });
 };

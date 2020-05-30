b0ebbd3ab36fb2c9190717062ce10c5d4a8453d1
SMoore3773 <Smooresculpture@gmail.com>
2020-05-30 12:23:15 -0400

    project setup II

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const resourceRouter = require('./routers/resourcesRouter/resourcesRouter');
+const projectsRouter = require('./routers/projectsRouter/projectsRouter');
 
 
 const server = express();
 
 server.use(express.json());
 
+server.use('/api/resources', resourceRouter);
+server.use('/api/projects', projectsRouter);
 
+server.get('/')
 module.exports = server;

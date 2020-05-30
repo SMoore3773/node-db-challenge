21e7e2ffa46f7aaa05d840bd20d74240ca8ae79e
SMoore3773 <Smooresculpture@gmail.com>
2020-05-30 13:04:53 -0400

    database table add migration set

--- data/migrations/20200530121609_table-create.js
+++ data/migrations/20200530121609_table-create.js
@@ -1,8 +1,54 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('projects', tbl =>{
+    tbl.increments('id');
+    tbl.string('project_name').notNullable();
+    tbl.string('project_desc');
+    tbl.boolean('project_complete').defaultTo('false');
+  })
+
+  .createTable('resources', tbl =>{
+    tbl.increments('id');
+    tbl.string('resource_name').notNullable();
+    tbl.string('resource_desc');
+  })
+
+  .createTable('tasks', tbl =>{
+    tbl.increments('id');
+    tbl.string('task_desc').notNullable();
+    tbl.string('task_notes');
+    tbl.boolean('task_complete').defaultTo('false');
+    tbl.integer('project_id')
+        .notNullable()
+        .unsigned()
+        .references('projects.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+  })
+
+  .createTable('project-resources', tbl =>{
+    tbl.integer('project_id')
+        .notNullable()
+        .unsigned()
+        .references('project.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    tbl.integer('resource_id')
+        .notNullable()
+        .unsigned()
+        .references('resources.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    tbl.primary(['project_id','resource_id'])
+  })
+
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('project-resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
 };
--- routers/resourcesRouter/resourcesRouter.js
+++ routers/resourcesRouter/resourcesRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const db = require('../../data/db-config');
+
 const router = express.Router();
 
 
